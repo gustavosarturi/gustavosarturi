6b2b3f970cb00cb492520fa25d4dfdc26a16b974
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2019-05-27 16:30:33 -0300

    Update Jacobi.jl

--- Julia/Análise Numérica/Jacobi.jl
+++ Julia/Análise Numérica/Jacobi.jl
@@ -13,8 +13,20 @@ function Jacobi(A, XO, b; max_tol = 10^(-8), max_it = 1000)
                 x[i] = (b[i]/A[i,i]) - sum
             end
         end
+
+        if norm(XO-x,Inf)/norm(x) < max_tol
+            return x, XO, it
+        end
         XO = x
         it += 1
     end
-    return x, XO, it
+
+    error("Falha!")
 end               
+
+using LinearAlgebra
+A = [10 -1 2 0; -1 11 -1 3; 2 -1 10 -4; 0 3 -1 8]
+b = [6; 25; -11; 15]
+x = [0; 0; 0; 0]
+
+Jacobi(A, x, b)
