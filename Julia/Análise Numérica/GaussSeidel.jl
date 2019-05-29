040fdbc06029061f3312cd38a166f4b750c47157
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2019-05-29 05:54:16 -0300

    Update GaussSeidel.jl

--- Julia/Análise Numérica/GaussSeidel.jl
+++ Julia/Análise Numérica/GaussSeidel.jl
@@ -1,16 +1,8 @@
-using LinearAlgebra
-A = [10 -1 2 0; -1 11 -1 3; 2 -1 10 -4; 0 3 -1 8]
-b = [6; 25; -11; 15]
-x = [0; 0; 0; 0]
-
-Jacobi(A, x, b)
-
-function GaussSeidel(A, XO, b; max_tol = 10^(-8), max_it = 1000)
+function Gauss_Seidel(A, XO, b; max_tol = 10^(-10), max_it = 1000)
     n = length(XO)
     x = zeros(n)
-    stop = false
     it = 1
-    while it < max_it && stop == false
+    while it < max_it
         for i=1:n
             sum = 0
             for j = 1:i-1
@@ -19,12 +11,16 @@ function GaussSeidel(A, XO, b; max_tol = 10^(-8), max_it = 1000)
             for j = i+1:n
                 sum += A[i,j] * XO[j]
             end
-            x[i] = (b[i] - sum)/A[i,i]
+                x[i] = 1/A[i,i] * ((b[i]) - sum)
         end
-        XO = x
+        
+        print("it = ", it, "=", x,"\n")
+
+        if norm(XO-x,Inf)/norm(x, Inf) < max_tol
+            return x, XO, it
+        end
+        XO .= x
         it += 1
     end
-    return x, XO, it
-end      
-
-GaussSeidel(A, x, b)
+    error("Falha")
+end
