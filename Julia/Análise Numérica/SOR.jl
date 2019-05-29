c006d981f69488277daaf292e44872fe8245ac27
Gustavo Sarturi <34655704+gustavosarturi@users.noreply.github.com>
2019-05-29 05:54:53 -0300

    Create SOR.jl

---
+++ Julia/Análise Numérica/SOR.jl
@@ -0,0 +1,24 @@
+function SOR(A, XO, b, ω; max_tol = 10^(-10), max_it = 1000)
+    n = length(XO)
+    x = zeros(n)
+    it = 1
+    while it < max_it
+        for i=1:n
+            sum = 0
+            for j = 1:i-1
+                sum += A[i,j] * x[j]
+            end
+            for j = i+1:n
+                sum += A[i,j] * XO[j]
+            end
+                x[i] = (1-ω)*XO[i] + ω/A[i,i] * (b[i]-sum)
+        end
+
+        if norm(XO-x,Inf)/norm(x, Inf) < max_tol
+            return x, XO, it
+        end
+        XO .= x
+        it += 1
+    end
+    error("Falha")
+end
